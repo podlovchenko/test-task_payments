6c450643fcc15b526e9b284a32a9047b6f2c7376
podlovchenko <podlovchenko.vera@gmail.com>
2021-05-13 10:42:40 +0300

    fix

--- app/microservices/accounts/app.module.ts
+++ app/microservices/accounts/app.module.ts
@@ -22,7 +22,7 @@ import {
     imports: [
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'postgresql-trapezoidal-19683',
+            host: process.env.DATABASE_URL || 'localhost',
             port: 5432,
             username: 'postgres',
             password: 'postgres',
